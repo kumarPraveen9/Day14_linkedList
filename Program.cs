65f15b6eea17555e13fe83ecef7947ddaab1208a
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-11 17:49:10 +0530

    inserting 30 between 56 and 70

--- Program.cs
+++ Program.cs
@@ -9,9 +9,14 @@ namespace day14LinkedLst
             Console.WriteLine("Current Size of the list is: " + list.getListSize());
 
             list.insertNode(56);
-            list.insertNode(30);
+          
             list.insertNode(70);
             list.printNodes();
+            
+            Console.WriteLine("Adding 25 at 2nd Location");
+            list.insertNth(30, 1);
+            list.printNodes(); 
+             
         }
     }       
 
