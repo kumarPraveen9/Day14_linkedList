51a8fee68644e019c2dd281e7d5ffff95de7457a
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-11 17:57:48 +0530

    main program updated!!

--- Program.cs
+++ Program.cs
@@ -9,13 +9,14 @@ namespace day14LinkedLst
             Console.WriteLine("Current Size of the list is: " + list.getListSize());
 
             list.insertNode(56);
-          
+            list.insertNode(30);
             list.insertNode(70);
             list.printNodes();
             
-            Console.WriteLine("Adding 30 at 1st Location");
-            list.insertNth(30, 1);
-            list.printNodes(); 
+           
+            Console.WriteLine("Deleting first node");
+            list.deleteFirstNode();
+            list.printNodes();
              
         }
     }       
