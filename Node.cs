525d21a4f2e9e237640fd40bf021f29acb419b23
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-11 17:31:36 +0530

    Node model class

---
+++ Node.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day14LinkedLst
+{
+   
+        public class Node
+        {
+            public int data;
+            public Node next;
+        }
+
+    
+}
