fc6485968e0fd152459c0d6d039b6e0e287e2443
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-11 19:12:37 +0530

    Main class ordered

--- Program.cs
+++ Program.cs
@@ -13,12 +13,9 @@ namespace day14LinkedLst
             list.insertNode(40);
             list.insertNode(70);
             list.printNodes();
-            
-            
-            Console.WriteLine("Current Size of the list is: " + list.getListSize());
-            Console.WriteLine("Deleting node from 2nd location");
-            list.deleteNthNode(2);
-            list.printNodes();
+
+            Console.WriteLine();
+            list.orderNodes();
         }
     }       
 
